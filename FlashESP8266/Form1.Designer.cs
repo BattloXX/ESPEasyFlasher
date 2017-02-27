51efdbca99649a28e0cb516aea34f628f55216fe
johan <johan@DESKTOP-CBKO5U1>
2017-02-27 21:50:25 +0100

    Add project files.

---
+++ FlashESP8266/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FlashESP8266
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttn_flash = new System.Windows.Forms.Button();
+            this.lbl_serial = new System.Windows.Forms.Label();
+            this.cbx_serial = new System.Windows.Forms.ComboBox();
+            this.lbl_firmware = new System.Windows.Forms.Label();
+            this.cbx_firmware = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // bttn_flash
+            // 
+            this.bttn_flash.Location = new System.Drawing.Point(302, 140);
+            this.bttn_flash.Name = "bttn_flash";
+            this.bttn_flash.Size = new System.Drawing.Size(75, 23);
+            this.bttn_flash.TabIndex = 0;
+            this.bttn_flash.Text = "Flash";
+            this.bttn_flash.UseVisualStyleBackColor = true;
+            this.bttn_flash.Click += new System.EventHandler(this.bttn_flash_Click);
+            // 
+            // lbl_serial
+            // 
+            this.lbl_serial.AutoSize = true;
+            this.lbl_serial.Location = new System.Drawing.Point(12, 37);
+            this.lbl_serial.Name = "lbl_serial";
+            this.lbl_serial.Size = new System.Drawing.Size(53, 13);
+            this.lbl_serial.TabIndex = 1;
+            this.lbl_serial.Text = "COM-Port";
+            // 
+            // cbx_serial
+            // 
+            this.cbx_serial.FormattingEnabled = true;
+            this.cbx_serial.Location = new System.Drawing.Point(88, 34);
+            this.cbx_serial.Name = "cbx_serial";
+            this.cbx_serial.Size = new System.Drawing.Size(289, 21);
+            this.cbx_serial.TabIndex = 2;
+            // 
+            // lbl_firmware
+            // 
+            this.lbl_firmware.AutoSize = true;
+            this.lbl_firmware.Location = new System.Drawing.Point(13, 74);
+            this.lbl_firmware.Name = "lbl_firmware";
+            this.lbl_firmware.Size = new System.Drawing.Size(49, 13);
+            this.lbl_firmware.TabIndex = 3;
+            this.lbl_firmware.Text = "Firmware";
+            // 
+            // cbx_firmware
+            // 
+            this.cbx_firmware.FormattingEnabled = true;
+            this.cbx_firmware.Location = new System.Drawing.Point(88, 74);
+            this.cbx_firmware.Name = "cbx_firmware";
+            this.cbx_firmware.Size = new System.Drawing.Size(289, 21);
+            this.cbx_firmware.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 175);
+            this.Controls.Add(this.cbx_firmware);
+            this.Controls.Add(this.lbl_firmware);
+            this.Controls.Add(this.cbx_serial);
+            this.Controls.Add(this.lbl_serial);
+            this.Controls.Add(this.bttn_flash);
+            this.Name = "Form1";
+            this.Text = "ESPEasy Flasher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttn_flash;
+        private System.Windows.Forms.Label lbl_serial;
+        private System.Windows.Forms.ComboBox cbx_serial;
+        private System.Windows.Forms.Label lbl_firmware;
+        private System.Windows.Forms.ComboBox cbx_firmware;
+    }
+}
+
---
+++ FlashESP8266/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlashESP8266
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (var port in ports)
+            {
+                cbx_serial.Items.Add(port);
+                cbx_serial.Text = port;
+            }
+
+            string[] fileArray = Directory.GetFiles(@".", "*.bin");
+            foreach (var files in fileArray)
+            {
+                //files.Replace(@".\", "");
+                cbx_firmware.Items.Add(files);
+            }
+
+            cbx_firmware.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbx_serial.DropDownStyle = ComboBoxStyle.DropDownList;
+
+
+        }
+
+        private void bttn_flash_Click(object sender, EventArgs e)
+        {
+            string serial = this.cbx_serial.GetItemText(this.cbx_serial.SelectedItem);
+            string firmware = this.cbx_firmware.GetItemText(this.cbx_firmware.SelectedItem);
+
+            if (serial == "")
+            {
+                MessageBox.Show("Please select Com-Port!");
+            }
+            if (firmware == "")
+            {
+                MessageBox.Show("Please select Firmware!");
+            }
+
+            if (firmware != "" & serial != "")
+            {
+
+                string arg = "-vv -cd nodemcu -cb 115200 -cp " + serial + " -ca 0x00000 -cf " + firmware;
+                string cmd = "esptool.exe";
+
+                Process myProcess = null;
+
+                try
+                {
+                    // Start the process.
+                    myProcess = Process.Start(cmd, arg);
+
+                    // Display the process statistics until
+                    // the user closes the program.
+                    do
+                    {
+                        if (!myProcess.HasExited)
+                        {
+                            // Refresh the current process property values.
+                            myProcess.Refresh();
+
+                            if (myProcess.Responding)
+                            {
+                                
+                            }
+                            else
+                            {
+                               
+                            }
+                        }
+                    }
+                    while (!myProcess.WaitForExit(1000));
+
+                    if (myProcess.ExitCode != 0)
+                    {
+                        MessageBox.Show("Flash Failed");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Flash Complete");
+
+                    }
+
+
+                }
+                finally
+                {
+                    if (myProcess != null)
+                    {
+                        myProcess.Close();
+                    }
+                }
+
+            }
+
+        }
+    }
+}
